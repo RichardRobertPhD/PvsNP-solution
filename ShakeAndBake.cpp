f19debdf51bffc60461ed9163d2bd97b8b902252
RichardRobertPhD <41920112+RichardRobertPhD@users.noreply.github.com>
2018-07-30 13:01:23 -0400

    Create ShakeAndBake.cpp

---
+++ ShakeAndBake.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                          © 2009 Richard Robert, Ph.D.
+
+*******************************************************************************/
+
+#include<iostream> 
+ 
+using namespace std;
+
+int youre = 17;
+int first = 18; 
+
+int main()
+{
+    if (youre != first)
+    {
+        cout<<"You're last.";
+        return 0;
+    }
+}
